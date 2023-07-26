0fe7030948a63311e0420474d8deaccb263776aa
Özkan Ardil <ozkanardil@gmail.com>
2023-07-27 01:30:09 +0300

    Desktop application has been completed

---
+++ src/Signalr.desktop/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Signalr.desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtClient;
+        private System.Windows.Forms.ListBox lstItems;
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtClient = new System.Windows.Forms.TextBox();
+            this.lstItems = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(99, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(460, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(99, 36);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(460, 25);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send Message To All Clients";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(99, 65);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(460, 20);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // txtClient
+            // 
+            this.txtClient.ForeColor = System.Drawing.Color.Black;
+            this.txtClient.Location = new System.Drawing.Point(99, 118);
+            this.txtClient.Name = "txtClient";
+            this.txtClient.ReadOnly = true;
+            this.txtClient.Size = new System.Drawing.Size(460, 20);
+            this.txtClient.TabIndex = 3;
+            // 
+            // lstItems
+            // 
+            this.lstItems.FormattingEnabled = true;
+            this.lstItems.Location = new System.Drawing.Point(99, 142);
+            this.lstItems.Name = "lstItems";
+            this.lstItems.Size = new System.Drawing.Size(460, 56);
+            this.lstItems.TabIndex = 2;
+            this.lstItems.DoubleClick += new System.EventHandler(this.lstItems_Select_Client);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OutPut";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Selected Client";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Online clients";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "(Double click on the one of the online client to select)";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(460, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Send Message To Selected Client";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtClient);
+            this.Controls.Add(this.lstItems);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/Signalr.desktop/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Signalr.desktop
+{
+    public partial class Form1 : Form
+    {
+        private HubConnection _hubConnection;
+        private List<string> itemList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Replace "http://localhost:5000/chatHub" with the URL of your SignalR hub endpoint
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7051/chatHub?name=desktop")
+                .Build();
+
+            // Receive messages from the server and update the output field
+            _hubConnection.On<string>("ReceiveMessage", (message) =>
+            {
+                Invoke(new Action(() =>
+                {
+                    txtOutput.Text = message;
+                }));
+            });
+
+
+            _hubConnection.On<Dictionary<string, string>>("ReceiveClients", (clients) =>
+            {
+                Invoke(new Action(() =>
+                {
+                    itemList.Clear();
+                    
+                    foreach (KeyValuePair<string, string> item in clients)
+                    {
+                        string itemText = item.Value;
+                        itemList.Add(itemText);
+                    }
+                
+                    lstItems.Items.Clear();
+                    lstItems.Items.AddRange(itemList.ToArray());
+                }));
+            });
+
+            try
+            {
+                _hubConnection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error connecting to SignalR hub: {ex.Message}", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                try
+                {
+                    await _hubConnection.SendAsync("SendMessage", txtInput.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error sending message: {ex.Message}", "Send Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private async void lstItems_Select_Client(object sender, EventArgs e)
+        {
+            if (lstItems.SelectedItem != null)
+            {
+                string selectedItemText = lstItems.SelectedItem.ToString();
+                this.txtClient.Text = selectedItemText;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtClient.Text))
+            {
+                MessageBox.Show($"Please select a client", "Select a client", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else
+            {
+                try
+                {
+                    await _hubConnection.SendAsync("SendMessageToClient", txtClient.Text, txtInput.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error sending message: {ex.Message}", "Send Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
